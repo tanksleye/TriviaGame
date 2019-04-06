fa4ea2fa452183a3855b9588e0e49cc39cbe78fb
Jet <elesebatanksley@yahoo.com>
2019-04-05 20:57:51 -0500

    finished adding in the questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+var myQuestions = [
+    {
+        question: "What movie does Owen Wilsong and Lary the Cable Guy star in together?",
+        answers: {
+            a : "Finding Nemo",
+            b : "Cars",
+            c : "A Bugs Life",
+            d : "Up",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What movie was mad on November 14, 1998?",
+        answers: {
+            a : "Toy Story",
+            b : "Finding Nemo", 
+            c : "A Bugs Life",
+            d : "Coco", 
+        },
+        correctAnswer: "c", 
+    },
+    {
+        question: "what movie has made the most money?",
+        answers: {
+            a : "Wall-E",
+            b : "Brave",
+            c : "Ratatouille",
+            d : "Finding Dory",
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "According to Vox.com, which Pixar movie is the very best?",
+        answers: {
+            a : "The Incredibles",
+            b : "Toy Story", 
+            c : "Up",
+            d : "Monsters Inc.", 
+        },
+        correctAnswer: "a", 
+    },
+    {
+        question: "Which movie has the most sequels??",
+        answers: {
+            a : "Toy Stoy",
+            b : "Cars",
+            c : "The Incredibles",
+            d : "Finding Nemo",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which movie was the 22nd movie that Pixar made?",
+        answers: {
+            a : "Monsters Inc.",
+            b : "Coco", 
+            c : "Ratatouille",
+            d : "Finding Nemo", 
+        },
+        correctAnswer: "c", 
+    },
+  
+ 
+
+]
+console.log("https://www.filmsite.org/pixaranimations.html");
+console.log("https://www.vox.com/2015/11/25/9797714/pixar-movies-rankings");
+
+
+
